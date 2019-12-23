c6b4f54247169bf159e623916b5d7f6bf506fdb3
rodrigosakamoto <rodosakamoto@gmail.com>
2019-12-23 18:59:06 -0300

    add SearchBar

--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { FaSearch } from 'react-icons/fa';
+import { Container, SearchBar } from './styles';
 
 import Menu from '../../components/Menu';
 
@@ -8,6 +8,16 @@ export default function Faq() {
   return (
     <Container>
       <Menu />
+      <SearchBar>
+        <h1>FAQ - Perguntas Frequentes</h1>
+        <form>
+          <input type="text" placeholder="Busque por termo..." />
+          <button type="button">
+            <FaSearch size={20} color="#fff" />
+            <span>BUSCAR</span>
+          </button>
+        </form>
+      </SearchBar>
     </Container>
   );
 }
--- src/pages/Faq/styles.js
+++ src/pages/Faq/styles.js
@@ -1,6 +1,47 @@
 import styled from 'styled-components';
+import colors from '../../styles/colors';
 
 export const Container = styled.div`
   max-width: 1280px;
   margin: 0 auto;
+`;
+
+export const SearchBar = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: bold;
+  flex-wrap: wrap;
+
+  h1 {
+    font-size: 32px;
+    font-weight: bold;
+  }
+
+  form {
+    display: flex;
+    align-items: center;
+
+    input {
+      border-radius: 12px;
+      padding: 10px;
+      border: 1px solid #eee;
+      max-width: 400px;
+    }
+
+    button {
+      background: ${colors.blue};
+      color: #fff;
+      border: 0;
+      border-radius: 12px;
+      width: 120px;
+      padding: 10px;
+      margin: 10px;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      font-weight: bold;
+    }
+  }
 `;
